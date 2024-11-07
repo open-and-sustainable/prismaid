7bb95c2ce5d78cd3e69ffda372ee1836433608fd
Riccardo Boero <ribo@nilu.no>
2024-11-07 22:21:37 +0100

    Fix Darwin package with R-hub CI/CD

--- src/R_wrapper.c
+++ src/R_wrapper.c
@@ -2,8 +2,6 @@
 #include <Rinternals.h>
 #include "_cgo_export.h"  // Header to access Go functions
 
-extern char* __cdecl RunReviewR(char* input);
-
 SEXP RunReviewR_wrap(SEXP input) {
     const char *c_input = CHAR(STRING_ELT(input, 0));
     const char *c_result = RunReviewR((char *)c_input);  // Call the Go function
--- src/_cgo_export.h
+++ src/_cgo_export.h
@@ -3,8 +3,6 @@
 #ifndef CGO_EXPORT_H
 #define CGO_EXPORT_H
 
-extern char* __cdecl RunReviewR(char* input);
-
 // Declaration of Go function exposed to C
 char* RunReviewR(char* input);
 
