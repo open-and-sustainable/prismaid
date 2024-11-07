0f0f95b6a4541d248a50e52a2d4b8a1dc640ee7a
Riccardo Boero <ribo@nilu.no>
2024-11-07 21:54:24 +0100

    Fix Windows package with R-hub CI/CD

--- src/_cgo_export.h
+++ src/_cgo_export.h
@@ -3,7 +3,20 @@
 #ifndef CGO_EXPORT_H
 #define CGO_EXPORT_H
 
-// Declaration of Go function exposed to C
-char* RunReviewR(char* input);
+#ifdef _WIN32
+    #define EXPORT __declspec(dllimport)
+#else
+    #define EXPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+EXPORT const char* __stdcall RunReviewR(char* input);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CGO_EXPORT_H
