6e2f24c156a32e65b9047b3d012169997a97c943
Riccardo Boero <ribo@nilu.no>
2024-11-08 09:52:19 +0100

    Fix packages to work on r-universe CI/CD

--- src/R_wrapper.c
+++ src/R_wrapper.c
@@ -1,13 +1,16 @@
-#ifdef HAVE_R_H
-#include <R.h>
-#include <Rinternals.h>
-#endif
 #include "_cgo_export.h"  // Header to access Go functions
 
+typedef void* SEXP;
+
+extern SEXP mkString(const char* str);
+extern void PROTECT(SEXP);
+extern void UNPROTECT(int count);
+
 SEXP RunReviewR_wrap(SEXP input) {
-    const char *c_input = CHAR(STRING_ELT(input, 0));
+    const char *c_input = (const char*)input;  // Cast input as a string
     const char *c_result = RunReviewR((char *)c_input);  // Call the Go function
-    SEXP result = PROTECT(mkString(c_result));
+    SEXP result = mkString(c_result);
+    PROTECT(result);
     UNPROTECT(1);
     return result;
 }
