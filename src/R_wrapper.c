88de85f669598f2bb17572d87819d9b1e520693a
Riccardo Boero <ribo@nilu.no>
2024-11-08 12:08:33 +0100

    Fix packages to work on r-universe CI/CD

--- src/R_wrapper.c
+++ src/R_wrapper.c
@@ -2,19 +2,19 @@
 
 typedef void* SEXP;
 
-extern SEXP mkString(const char* str);
+// Manually define R API functions without including R headers
+extern SEXP Rf_mkString(const char* str);
+extern SEXP Rf_protect(SEXP);
+extern void Rf_unprotect(int count);
 
 // Manually define PROTECT and UNPROTECT without including R headers
 #define PROTECT(s) Rf_protect(s)
 #define UNPROTECT(n) Rf_unprotect(n)
 
-extern SEXP Rf_protect(SEXP);
-extern void Rf_unprotect(int count);
-
 SEXP RunReviewR_wrap(SEXP input) {
     const char *c_input = (const char*)input;  // Cast input as a string
     const char *c_result = RunReviewR((char *)c_input);  // Call the Go function
-    SEXP result = mkString(c_result);
+    SEXP result = Rf_mkString(c_result);
     PROTECT(result);
     UNPROTECT(1);
     return result;
