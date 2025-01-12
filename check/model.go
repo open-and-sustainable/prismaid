d5beb923943fe862a57b3688f98b484556b62d93
Riccardo Boero <ribo@nilu.no>
2025-01-12 21:15:26 +0100

    add Cohere command-r7b

--- check/model.go
+++ check/model.go
@@ -91,10 +91,10 @@ func getGoogleAIModel(prompt string, modelName string, key string) string {
 }
 
 func getCohereModel(prompt string, modelName string, key string) string {
-	model := "command-r"
+	model := "command-r7b-12-2024"
 	switch modelName {
 	case "": 
-		// cost optimization, command-r is currently the cheapest and with the most input tokens allowed
+		// cost optimization, command-r7b is currently the cheapest and with the most input tokens allowed
 	case "command": // leave the model selected by the user, but chek if supported
 		model = modelName
 	case "command-light":
